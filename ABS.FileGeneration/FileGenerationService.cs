28b6a3c67b5691489d2acb22b89282b8fb3551cd
Timothy Jewell <timothydjewell@gmail.com>
2023-06-27 15:10:48 -0700

    Initial commit of ABS repo

---
+++ ABS.FileGeneration/FileGenerationService.cs
@@ -0,0 +1,10 @@
+﻿namespace ABS.FileGeneration
+{
+    public class FileGenerationService
+    {
+        public Task<FileStream> GenerateFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
